ea98667119fdb9e3262b1912a1bb2fb6691e493d
Raihan-710083 <710083@student.inholland.nl>
2025-10-22 20:05:44 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module raihan.inholland.nl.end_assignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens raihan.inholland.nl.end_assignment to javafx.fxml;
+    exports raihan.inholland.nl.end_assignment;
+}
