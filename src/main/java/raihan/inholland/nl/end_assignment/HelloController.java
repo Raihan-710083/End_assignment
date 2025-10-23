e2b93a6bf28c490c8a34ac240cea161f146fd3cf
Raihan-710083 <710083@student.inholland.nl>
2025-10-23 12:33:32 +0200

    added a simple ui

--- src/main/java/raihan/inholland/nl/end_assignment/HelloController.java
+++ src/main/java/raihan/inholland/nl/end_assignment/HelloController.java
@@ -1,6 +1,7 @@
 package raihan.inholland.nl.end_assignment;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -8,7 +9,18 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Button helloButton;
+
+    @FXML
+    private void initialize() {
+        welcomeText.setText("Java Quiz Game");
+        helloButton.setText("Start Application");
+    }
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to Quiz Game!");
+        helloButton.setText("JavaFX Works!");
+        System.out.println("JavaFX is working correctly!");
     }
 }
